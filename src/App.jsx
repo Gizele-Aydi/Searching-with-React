28c022e7e7da30b19228243cfad9da54de28418e
Gizele-Aydi <aydigizele@tbs.u-tunis.tn>
2024-03-01 14:57:32 +0100

    W5 Arrow Functions in React

--- src/App.jsx
+++ src/App.jsx
@@ -19,43 +19,37 @@ const list = [
   },
 ];
 
-function List() {
-  return (
+const List = () => (
     <ul>
-        {list.map(function(item) {
-            return ( 
-                <li key={item.objectID}>{item.title}
-                    <span>
-                        <a href="{item.url}">{item.title}</a>
-                    </span>
-                    <span>{item.author}</span>
-                    <span>{item.num_comments}</span>
-                    <span>{item.points}</span>
-                </li>
-            );
-         })}
+        {list.map((item) => (
+            <li key={item.objectID}>{item.title}
+                <span>
+                    <a href="{item.url}">{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+            </li>
+        ))}
     </ul>
-  );
-}
+);
 
-function Search() {
-  return(
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-    </div>
-  );
-}
+const Search = () => {
+    return (
+        <div>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" />
+        </div>
+    );
+};
 
-function App(){
-  return (
+const App = () => (
     <div>
         <h1>My hacker stories</h1>
         <Search />
         <hr />
         <List />
     </div>
-  );
-}
+);
 
 export default App;
