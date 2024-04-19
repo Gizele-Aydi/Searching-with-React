267434ec13fb9ad93f0744cea317bede3ba82c93
Gizele-Aydi <aydigizele@tbs.u-tunis.tn>
2024-04-19 14:37:59 +0100

    W6 Adding props

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
 import * as React from "react"
 
-const list = [
-  {
-    title: "React ",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux ",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const List = () => (
+const List = (props) => (
     <ul>
-        {list.map((item) => (
-            <li key={item.objectID}>{item.title}
+        {props.list.map((item)=> (
+            <li key={item.objectID}>
                 <span>
                     <a href="{item.url}">{item.title}</a>
                 </span>
@@ -47,14 +28,33 @@ const Search = () => {
         </div>
     );
 };
-
-const App = () => (
-    <div>
-        <h1>My hacker stories</h1>
-        <Search />
-        <hr />
-        <List />
-    </div>
-);
-
+const App = () => {
+    const stories = [
+        {
+          title: "React ",
+          url: "https://reactjs.org/",
+          author: "Jordan Walke",
+          num_comments: 3,
+          points: 4,
+          objectID: 0,
+        },
+        {
+          title: "Redux ",
+          url: "https://redux.js.org/",
+          author: "Dan Abramov, Andrew Clark",
+          num_comments: 2,
+          points: 5,
+          objectID: 1,
+        },
+      ];
+    return (
+        <div>
+            <h1>My hacker stories</h1>
+            <Search />
+            <hr />
+            <List list={stories} />
+        </div>
+    );
+};
+    
 export default App;
