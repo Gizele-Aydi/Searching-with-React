a8fba84b9bd5f6aa4de7b84ce1bf8ab32a85cc29
Gizele-Aydi <aydigizele@tbs.u-tunis.tn>
2024-04-19 15:34:44 +0100

    W6 Adding React state for the searchTerm

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,55 @@
 import * as React from "react"
 
-const List = (props) => (
-    <ul>
-        {props.list.map((item)=> (
-            <li key={item.objectID}>
-                <span>
-                    <a href="{item.url}">{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-            </li>
-        ))}
-    </ul>
-);
-
 const Search = () => {
+    console.log("Search renders")
+    const [searchTerm, setSearchTerm] = React.useState('');
+
     const handleChange = (event) => {
-        console.log(event);
-        console.log(event.target.value);
+        setSearchTerm(event.target.value);
     };
     
     return (
         <div>
             <label htmlFor="search">Search: </label>
             <input id="search" type="text" onChange={handleChange} />
+
+            <p>
+                Searching for <strong>{searchTerm}</strong>.
+            </p>
         </div>
     );
 };
+
+const List = (props) => {
+    console.log("List renders")
+    return (
+    <ul>
+        {props.list.map((item)=> (
+            <Item key={item.objectID} item={item} />
+        ))}
+    </ul>
+)};
+
+const Item = (props) => {
+    console.log("Item renders")
+    return (
+        <li>
+            <span>
+                <a href="{props.item.url}">{props.item.title}</a>
+            </span>
+            <span>{props.item.author}</span>
+            <span>{props.item.num_comments}</span>
+            <span>{props.item.points}</span>
+        </li>
+)};
+
 const App = () => {
+    console.log("App renders")
     const stories = [
         {
           title: "React ",
           url: "https://reactjs.org/",
-          author: "Jordan Walke",
+          author: "Jordan Walke ",
           num_comments: 3,
           points: 4,
           objectID: 0,
@@ -41,7 +57,7 @@ const App = () => {
         {
           title: "Redux ",
           url: "https://redux.js.org/",
-          author: "Dan Abramov, Andrew Clark",
+          author: "Dan Abramovs, Andrew Clark ",
           num_comments: 2,
           points: 5,
           objectID: 1,
