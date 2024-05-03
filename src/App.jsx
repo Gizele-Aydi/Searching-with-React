2c3c590686c40068466847277ca10af066fa594d
Gizele-Aydi <aydigizele@tbs.u-tunis.tn>
2024-05-03 15:33:25 +0100

    W8: React inline handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,71 @@
 import * as React from "react"
 
-const List = ({list}) => (
+const List = ({ list, onRemoveItem }) => (
     <ul>
         {list.map((item)=> (
-            <Item key={item.objectID} item={item} />
+            <Item 
+            key={item.objectID} 
+            item={item}
+            onRemoveItem={onRemoveItem} 
+            />
         ))}
     </ul>
 );
 
-const Item = ({item}) => (
+const Item = ({ item, onRemoveItem }) => {
+    return (
     <li>
         <span>
-            <a href="{item.url}">{item.title}</a>
+            <a href={item.url}>{item.title}</a>
         </span>
         <span>{item.author}</span>
         <span>{item.num_comments}</span>
         <span>{item.points}</span>
+        <span>
+            <button 
+            type='button' 
+            onClick={() => onRemoveItem(item)}>
+                Dismiss
+            </button>
+        </span>
     </li>
-);
+    );
+};
+
+const initialStories = [
+    {
+      title: "React ",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke ",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux ",
+      url: "https://redux.js.org/",
+      author: "Dan Abramovs, Andrew Clark ",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+];
 
 const App = () => {
-    const stories = [
-        {
-          title: "React ",
-          url: "https://reactjs.org/",
-          author: "Jordan Walke ",
-          num_comments: 3,
-          points: 4,
-          objectID: 0,
-        },
-        {
-          title: "Redux ",
-          url: "https://redux.js.org/",
-          author: "Dan Abramovs, Andrew Clark ",
-          num_comments: 2,
-          points: 5,
-          objectID: 1,
-        },
-    ];
+
     const [searchTerm, setSearchTerm] = React.useState(
         localStorage.getItem('search') || 'React'
     );
+    
+    const [stories, setStories] = React.useState(initialStories);
 
+    const handleRemoveStory = (item) => {
+        const newStories = stories.filter(
+            (story) => item.objectID !== story.objectID
+        );
+        setStories(newStories);
+    };
+    
     React.useEffect(() => {
         localStorage.setItem('search', searchTerm);
     }, [searchTerm]);
@@ -65,7 +89,7 @@ const App = () => {
             <strong>Search: </strong>
             </InputWithLabel>
             <hr />
-            <List list={searchedStories} />
+            <List list={searchedStories} onRemoveItem={handleRemoveStory} />
         </div>
     );
 };
